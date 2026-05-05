a72b9b9b31608dee97d8e463b44a113af4e09399
cachu94 <fgiacri.94@gmail.com>
2026-05-05 00:03:19 -0300

    avance 4/5/2026

--- README.txt
+++ README.txt
@@ -1,20 +1,16 @@
-Trabajo Práctico 01: JavaScript 
-Aplicación web tipo SPA que permita buscar álbumes, visualizar información y gestionar 
-favoritos utilizando la API de Discogs. 
-Fase 1: Datos y persistencia 
-Puesta en común 29/04/2026 
-Objetivo 
-Trabajar con datos, asincronía y almacenamiento. 
-Requisitos 
-● Implementar funciones para consumir la API: 
-○ búsqueda de álbumes 
-○ obtención de detalle 
-● Transformar los datos recibidos (no usar la respuesta cruda, filtrar datos importantes) 
-● Implementar localStorage: 
-○ guardar favoritos 
-○ recuperar favoritos al iniciar 
-Condiciones 
-● Manejar errores en las peticiones 
-● Separar lógica en funciones reutilizables 
-● Mostrar datos por la terminal 
-● Implementar todo en funciones
+Trabajo Práctico 01: JavaScript
+Aplicación web tipo SPA que permita buscar álbumes, visualizar información y gestionar favoritos utilizando la API de Discogs.
+Fase 2: Renderizado (SPA)
+Puesta en común 06/05/2026
+Objetivo
+ Mostrar información en pantalla de forma dinámica en una sola página.
+Requisitos
+Mostrar resultados de búsqueda en el HTML
+Mostrar detalles de los álbumes
+Mostrar lista de favoritos
+Simular navegación sin cambiar de ruta
+
+Condiciones
+Un solo index.html
+Todo el render ocurre dentro de un contenedor principal
+Separar funciones de render (lista, tarjeta, detalle)
--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const home = document.getElementById('homeBtn');
-const perfil = document.getElementById('perfilBtn');
+const botonHome = document.getElementById('homeBtn');
+const botonMiPerfil = document.getElementById('perfilBtn');
 const buscador = document.getElementById('buscadorInput');
-const buscarBtn = document.getElementById('buscadorBtn');
+const botonBuscar = document.getElementById('buscadorBtn');
 const contenedorAlbums = document.getElementById('contenedoralbums');
+let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
 
 const DISCOGS_TOKEN = 'TVysyskeJLHLCBNKQWIkVGDJWFhMaUWGhIIHtRak';
 const BASE_URL = 'https://api.discogs.com';
 const URL_SEARCH = `${BASE_URL}/database/search`;
 const URL_MASTER = `${BASE_URL}/masters`;
 
-buscarBtn.addEventListener('click', () => {
+// Al hacer click en el botón de búsqueda, se obtiene el valor del input y se inicia la búsqueda
+botonBuscar.addEventListener('click', () => {
     const query = buscador.value.trim();
     if (!query) return alert('Por favor, ingresa un artista o álbum.');
     buscarAlbums(query);
     });
 
+// Si la busqueda se realiza, se muestra un mensaje de "Buscando..." mientras se obtienen los datos de la API. Luego, se transforman los datos para mostrar solo lo necesario (id, master_id, título y portada) y se renderizan las tarjetas de álbumes en el contenedor correspondiente. Al hacer click en el botón "Ver Detalles" de cada tarjeta, se obtiene información adicional del álbum (géneros, estilos, año y tracklist) y se muestra debajo de la tarjeta. Si ocurre algún error durante la búsqueda o al obtener detalles, se muestra un mensaje de error en el contenedor.
 async function buscarAlbums(query) {
     contenedorAlbums.innerHTML = '<p>Buscando...</p>'
     try {
-        const url = `${URL_SEARCH}?q=${query}&type=master&per_page=10&token=${DISCOGS_TOKEN}`;
+        const url = `${URL_SEARCH}?q=${query}&type=master&per_page=20&token=${DISCOGS_TOKEN}`;
         const res = await fetch(url);
         if (!res.ok) throw new Error('Error en la busqueda ...');
         const data = await res.json();
@@ -60,32 +63,54 @@ function renderizarAlbums(albums) {
     contenedorAlbums.innerHTML = '';
 
     albums.forEach(album => {
+        const esFav = favoritos.some(fav => fav.master_id === album.master_id);
+        const colorInicial = esFav ? 'text-red-500' : 'text-gray-500'
+
+
         const card = document.createElement('article');
-        card.classList.add('card-album');
+        card.className = "card bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 p-4 flex flex-col items-center text-center";
+
+        const imagen = album.portada || album.cover_image
+        const titulo = album.titulo || album.title
+        const masterId = album.master_id
 
         card.innerHTML = `
-        <img src="${album.portada}" alt="${album.titulo}">
-        <h2>${album.titulo}</h2>
-        <button class="detallesBtn" data-id="${album.master_id}">Ver Detalles</button>
-        <button class="favBtn" data-id="${album.id}">&#x2665</button>
-        <div class="info-extra" id="extra-${album.master_id}" style="display: none;"></div>
+        <img src="${imagen}" class="w-full h-full object-cover rounded-md mb-4 shadow-sm">
+        <h2 class="text-lg font-bold text-slate-800 mb-2 h-14 overflow-hidden">${titulo}</h2>
+        <div class="flex gap-2 w-full justify-center mt-auto">
+            <button class="detallesBtn bg-slate-700 text-white px-3 py-1 rounded-full text-sm hover:bg-slate-900 transition-colors">Ver Detalles</button>
+            <button class="botonFav text-2xl ${colorInicial}">&#x2665;</button>
+        </div>
+        <div class="info-extra" id="extra-${masterId}" style="display: none;"></div>
         `;
 
-        card.querySelector('.detallesBtn').addEventListener('click', async (e) => {
-            const extraDiv = document.getElementById(`extra-${album.master_id}`);
-            if (extraDiv.innerHTML !== "") {
-                extraDiv.style.display = extraDiv.style.display === 'none' ? 'block' : 'none';
-                return;
-            }
+        // Evento Favoritos
+        const btnFav = card.querySelector(".botonFav");
+        btnFav.addEventListener('click', () => {
+            toggleFavorito(album, btnFav);
+        });
 
-            const detalle = await obtenerDetallesMaster(album.master_id);
+        // Evento de Detalles
+        const btnDetalles = card.querySelector('.detallesBtn');
+
+        btnDetalles.addEventListener('click', async () => {
+            const extraDiv = document.getElementById(`extra-${masterId}`);
+            if (extraDiv.style.display === 'block') {
+                extraDiv.style.display = 'none';
+                return    
+            }
+            
+            extraDiv.innerHTML = '<p>Cargando ...</p>';
+            extraDiv.style.display = 'block';
+
+            const detalle = await obtenerDetallesMaster(masterId);
             if (detalle) {
-                const cancionesHTML = detalle.tracklist.map(t => `<li>${t.position} - ${t.title} (${t.duration})</li>`).join('');
+                const cancionesHTML = detalle.tracklist.map(t => `<li class="mt-2">${t.position} - ${t.title} (${t.duration})</li>`).join('');
                 extraDiv.innerHTML = `
-                <p>Géneros: ${detalle.generos}</p>
-                <p>Estilos: ${detalle.estilos}</p>
-                <p>Año: ${detalle.año}</p>
-                <ul>${cancionesHTML}</ul>
+                <p><strong>Géneros:</strong> ${detalle.generos}</p>
+                <p><strong>Estilos:</strong> ${detalle.estilos}</p>
+                <p><strong>Año:</strong> ${detalle.año}</p>
+                <ul class="mt-4">${cancionesHTML}</ul>
                 `;
             }
         });
@@ -93,3 +118,41 @@ function renderizarAlbums(albums) {
     });
 
 }
+
+function toggleFavorito(album, btn) {
+    const index = favoritos.findIndex(fav => fav.master_id === album.master_id);
+
+    if (index > -1) {
+        favoritos.splice(index, 1);
+        btn.classList.replace('text-red-500', 'text-gray-500');
+    } else {
+        favoritos.push(album);
+        btn.classList.replace('text-gray-500', 'text-red-500');
+    }
+
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+}
+
+
+// PERFIL
+botonMiPerfil.addEventListener('click', () => {
+    renderizarAlbums(favoritos);
+});
+
+botonHome.addEventListener("click", async () => {
+    async function buscarAlbumsAlAzar() {
+        contenedorAlbums.innerHTML = '<p class="text-slate-500 text-center w-full">Cargando álbumes...</p>';
+        try {
+            const randomPage = Math.floor(Math.random() * 100) + 1; // Obtener una página aleatoria para variar los resultados
+            const url = `${URL_SEARCH}?&type=master&page=${randomPage}&per_page=20&token=${DISCOGS_TOKEN}`;
+            const res = await fetch(url);
+            if (!res.ok) throw new Error('Error al cargar álbumes');
+            const data = await res.json();
+            renderizarAlbums(data.results);
+        } catch (err) {
+            contenedorAlbums.innerHTML = `<p class="text-red-600 text-center">${err.message}</p>`;
+        }
+    }
+
+    buscarAlbumsAlAzar();
+});
